132923b57c9e30f4734b9c5c630ddce5bd0f733e
avarayr <7735415+avarayr@users.noreply.github.com>
2025-10-24 17:33:07 -0400

    Apply suggestion from @devnoname120
    Co-authored-by: Paul <devnoname120@gmail.com>

--- lib/apps/framer.ts
+++ lib/apps/framer.ts
@@ -3,9 +3,9 @@ import { FixedStatus, type AppMeta } from "../../types";
 
 export const Framer: AppMeta = {
   icon: "https://cdn.brandfetch.io/idCeIE9B96/w/400/h/400/theme/dark/icon.jpeg?c=1bxid64Mup7aczewSAYMX&t=1667565216026",
-  id: “framer-stable",
-  friendlyName: “Framer",
-  twitter: “framer",
+  id: "framer-stable",
+  friendlyName: "Framer",
+  twitter: "framer",
   async checkIsFixed() {
     const url = "https://updates.framer.com/electron/darwin/arm64/Framer.zip";
     const pat = "_cornerMask";
