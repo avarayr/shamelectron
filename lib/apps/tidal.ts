2edb89044b152e76dbe586f9f7e28b07321ed934
AllDaGearNoIdea <AllDaGear@No.Idea>
2025-11-03 11:42:12 +0000

    Update friendly name to 'Tidal'

--- lib/apps/tidal.ts
+++ lib/apps/tidal.ts
@@ -4,7 +4,7 @@ import { FixedStatus, type AppMeta } from "../../types";
 export const Tidal: AppMeta = {
   icon: "https://cdn.brandfetch.io/idtVfiVkt8/w/400/h/400/theme/dark/icon.jpeg?c=1bxid64Mup7aczewSAYMX&t=1756402137207",
   id: "tidal",
-  friendlyName: "TIDAL",
+  friendlyName: "Tidal",
   twitter: "TIDAL",
   async checkIsFixed() {
     const url = "https://desktop.tidal.com/download/mac";
